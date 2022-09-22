6ec99ab1393f11ae42b3040c213f6d2222dc835d
Lawrence Chan <lawrence.c@hotmail.co.uk>
2022-09-22 10:56:19 -0700

    Implement a query function that takes in an object paramter as filter

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -109,14 +109,48 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = (options, limit = 10) => {
-  
-  return pool
-    .query(`
-      SELECT *
-      FROM properties
-      LIMIT $1;
-      `,
-    [limit])
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
+  `;
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `
+    AND city LIKE $${queryParams.length}
+    `;
+  }
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `
+    AND owner_id = $${queryParams.length}
+    `;
+  }
+  if (options.minimim_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimim_price_per_night * 100, options.maximum_price_per_night * 100);
+    queryString += `
+    AND cost_per_night >= $${queryParams.length - 1}
+    AND cost_per_night <= $${queryParams.length} 
+    `;
+  }
+  queryString += `
+  GROUP BY properties.id
+  `;
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `
+    HAVING avg(property_reviews.rating) >= $${queryParams.length}
+    `;
+  }
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  console.log(queryString, queryParams);
+  return pool.query(queryString,queryParams)
     .then((result) => {
       return result.rows;
     })
